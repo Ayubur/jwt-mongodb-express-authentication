2c15a4858fd08920843bb1e63a3525387741a377
Ayubur <ayubur.1994@gmail.com>
2019-10-23 12:08:45 +0600

    signin and protected routes implementation using passport

--- server/router.js
+++ server/router.js
@@ -1,5 +1,18 @@
 const authentication= require('./controllers/authentication');
-module.exports = (app)=>{
+const passportService = require('./services/passport');
+const passport = require('passport');
 
+
+const requireAuth = passport.authenticate('jwt',{session:false});
+const requireLogin = passport.authenticate('local',{session:false});
+
+
+module.exports = (app)=>{
+    
+    app.get("/",requireAuth, function(req,res){
+        res.send({"hi":"there"});
+    });
+
+    app.post('/login',requireLogin,authentication.signin);
     app.post("/signup", authentication.signup);
 }
