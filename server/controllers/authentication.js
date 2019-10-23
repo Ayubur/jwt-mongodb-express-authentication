2a10c60df3b509dfc91bbbf56b4ab6d8722cdf2d
Ayubur <ayubur.1994@gmail.com>
2019-10-23 12:08:45 +0600

    signin and protected routes implementation using passport

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -8,6 +8,12 @@ function tokenForUser(user){
     return jwt.encode({sub:user.id, iat:timestamp},config.secret);
 }
 
+
+
+exports.signin= function(req,res,next){
+    res.send({ token : tokenForUser(req.user) });
+}
+
 exports.signup = function(req,res,next){
     
     const email = req.body.email;
--- server/models/users.js
+++ server/models/users.js
@@ -28,7 +28,19 @@ userSchema.pre('save', function (next) {
             next();
         });
     });
- })
+ });
+
+ userSchema.methods.comparePassword= function(candidatePassword, callback){
+
+    bcrypt.compare(candidatePassword,this.password, function(err,isMatch){
+
+        if(err){
+            return callback(err);
+        }
+        callback(null,isMatch);
+    });
+ }
+
 //Define our model class
 
 const ModelClass = mongoose.model('users',userSchema);
